762944aa7aa27d3603d4224b3dade73ad7d84af9
nazver <jeka050616@gmail.com>
2026-02-02 18:18:04 +0700

    done 1

---
+++ gendiff.js
@@ -0,0 +1,14 @@
+import { Command } from "commander";
+
+const program = new Command();
+program
+  .name("gendiff")
+  .arguments("file1")
+  .arguments("file2")
+  .description("Compares two configuration files and shows a difference.")
+  .version("1.0.0")
+  .action((file1, file2) => {
+    console.log(file1, file2);
+  });
+
+program.parse(process.argv);
