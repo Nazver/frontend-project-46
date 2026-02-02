4e54949378616987023568a67412fc8103758a7d
nazver <jeka050616@gmail.com>
2026-02-02 19:09:26 +0700

    done3

--- gendiff.js
+++ gendiff.js
@@ -1,4 +1,5 @@
 import { Command } from "commander";
+import * as fs from "fs";
 
 const program = new Command();
 program
@@ -7,9 +8,11 @@ program
   .arguments("file2")
   .description("Compares two configuration files and shows a difference.")
   .version("1.0.0")
-  .option("-f, --format" , 'output format')
+  .option("-f, --format", "output format")
   .action((file1, file2) => {
-    console.log(file1, file2);
+    const filedata1 = fs.readFileSync(file1 ,'utf8');
+    const filedata2 = fs.readFileSync(file2, 'utf8');
+    console.log(JSON.parse(filedata1), JSON.parse(filedata2));
   });
 
 program.parse(process.argv);
