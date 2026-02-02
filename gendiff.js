e945b2af2a8ca0264fe291ed05ff3ab4e353a323
nazver <jeka050616@gmail.com>
2026-02-02 18:21:42 +0700

    done2

--- gendiff.js
+++ gendiff.js
@@ -7,6 +7,7 @@ program
   .arguments("file2")
   .description("Compares two configuration files and shows a difference.")
   .version("1.0.0")
+  .option("-f, --format" , 'output format')
   .action((file1, file2) => {
     console.log(file1, file2);
   });
