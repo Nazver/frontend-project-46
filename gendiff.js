2fd3cee184e4c3aa15189474b9a3557297d451bb
Bob <jeka050616@gmail.com>
2026-02-05 16:18:08 +0300

    done4

--- gendiff.js
+++ gendiff.js
@@ -10,9 +10,57 @@ program
   .version("1.0.0")
   .option("-f, --format", "output format")
   .action((file1, file2) => {
-    const filedata1 = fs.readFileSync(file1 ,'utf8');
-    const filedata2 = fs.readFileSync(file2, 'utf8');
-    console.log(JSON.parse(filedata1), JSON.parse(filedata2));
+   const filedata1 = JSON.parse(fs.readFileSync(file1, "utf8"));
+    const filedata2 = JSON.parse(fs.readFileSync(file2, "utf8"));
+    
+    const diffResult = diff(filedata1, filedata2);
+    console.log(formatDiff(diffResult));
   });
 
+function diff(file1, file2) {
+  const allKeys = new Set([...Object.keys(file1), ...Object.keys(file2)]);
+  const result = [];
+  
+  const sortedKeys = Array.from(allKeys).sort();
+  
+  for (const key of sortedKeys) {
+    const value1 = file1[key];
+    const value2 = file2[key];
+    
+    if (!(key in file1) && key in file2) {
+      result.push({ key, status: "added", value: value2 });
+    } else if (key in file1 && !(key in file2)) {
+      result.push({ key, status: "removed", value: value1 });
+    } else if (value1 !== value2) {
+   
+      result.push({ key, status: "changed", oldValue: value1, newValue: value2 });
+    } else { 
+      result.push({ key, status: 'unchanged', value: value1 });
+    }
+  }
+
+  return result;
+}
+function formatDiff(diffArray) {
+  const lines = [];
+  
+  diffArray.forEach(item => {
+    switch(item.status) {
+      case 'added':
+        lines.push(`  + ${item.key}: ${item.value}`);
+        break;
+      case 'removed':
+        lines.push(`  - ${item.key}: ${item.value}`);
+        break;
+      case 'changed':
+        lines.push(`  - ${item.key}: ${item.oldValue}`);
+        lines.push(`  + ${item.key}: ${item.newValue}`);
+        break;
+      case 'unchanged':
+        lines.push(`    ${item.key}: ${item.value}`);
+        break;
+    }
+  });
+    return `{\n${lines.join('\n')}\n}`;
+}
 program.parse(process.argv);
