18a3af4e240f4b4b8271a4987face7555e83581b
Bob <jeka050616@gmail.com>
2026-02-08 18:01:54 +0700

    done6

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import diff from './diff.js'
-import formatDiff from './formatDiff.js'
-import * as fs from 'node:fs'
+import diff from './diff.js';
+import formatDiff from './formatDiff.js';
+import parseFile from './parsers.js';
+
 export default function index(filePath1, filePath2) {
-  const filedata1 = JSON.parse(fs.readFileSync(filePath1, 'utf8'))
-  const filedata2 = JSON.parse(fs.readFileSync(filePath2, 'utf8'))
-  const diffResult = diff(filedata1, filedata2)
-  return formatDiff(diffResult)
+  const filedata1 = parseFile(filePath1);
+  const filedata2 = parseFile(filePath2);
+  const diffResult = diff(filedata1, filedata2);
+  return formatDiff(diffResult);
 }
